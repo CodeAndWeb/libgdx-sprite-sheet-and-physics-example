3d3152bda05b18f2a09181c40d30a9f21a421f9f
Joachim Grill <joachim@codeandweb.com>
2017-04-18 16:03:39 +0200

    new gdx project

---
+++ desktop/src/com/codeandweb/tutorials/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.codeandweb.tutorials.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.codeandweb.tutorials.PhysicsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PhysicsGame(), config);
+	}
+}
